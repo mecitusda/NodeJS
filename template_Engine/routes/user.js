56553f619218be0cd5ce99f07a160d1c8de191b3
Abdul Mecit Usda <92247286+mecitusda@users.noreply.github.com>
2024-06-21 00:01:22 +0300

    Template engine bitti.

--- template_Engine/routes/user.js
+++ template_Engine/routes/user.js
@@ -4,7 +4,29 @@ var i=1;
 
 const data={
     title:"Anasayfa",
-    categories:["Web Geliştirme","Mobil Uygulamalar","Veri Analizi","Programlamaw"]
+    categories:["Web Geliştirme","Mobil Uygulamalar","Veri Analizi","Programlama"],
+    blogs:[{
+        id:1,
+        title:"Komple Uygulamalı Web Geliştirme",
+        content:"Sıfırdan ileri seviyeye 'Web Geliştirme': Html, Css, Sass, Flexbox, Bootstrap, Javascript, Angular, JQuery, Asp.Net Mvc&Core Mvc",
+        picture:"1.jpeg",
+        isvisible:true,
+        verify:true,
+    },{
+        id:2,
+        title:"Python ile Sıfırdan İleri Seviye Python Programlama",
+        content:"Sıfırdan İleri Seviye Python Dersleri.Veritabanı,Veri Analizi,Bot Yazımı,Web Geliştirme(Django)",
+        picture:"2.jpeg",
+        isvisible:true,
+        verify:true,
+    },{
+        id:3,
+        title:"Sıfırdan İleri Seviye Modern Javascript Dersleri ES7+",
+        content:"Modern javascript dersleri ile (ES6 & ES7+) Nodejs, Angular, React ve VueJs için sağlam bir temel oluşturun.",
+        picture:"3.jpeg",
+        isvisible:true,
+        verify:true,  
+    }]
 }
 
 
